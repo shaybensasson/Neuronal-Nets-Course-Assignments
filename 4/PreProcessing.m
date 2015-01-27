eade8c4ce19ecb8a8ada1c71d9cb9701e9353659
shaybensasson <shaybensasson@gmail.com>
2015-01-28 00:59:15 +0200

    TTNonRep pre-processing into Neuron cell array

--- 4/PreProcessing.m
+++ 4/PreProcessing.m
@@ -2,14 +2,6 @@ if (~exist('TTNonRep','var'))
     load('FixedData.mat')
 end
 
-%Normalize stim time to start from 1
-StimTimeNonRepNorm=StimTimeNonRep-StimTimeNonRep(1);
-%ExperimentTicks=1:max(StimTimeNonRepNorm);
-%ExperimentTicks(ExperimentTicks==StimTimeNonRepNorm)
-%indexes = 1:length(StimTimeNonRepNorm);
-%ind = indexes(logical(StimTimeNonRepNorm(:)));
-
-
 SECONDS_IN_WINDOW = 100;
 TICKS_IN_SECOND = 10000;
 TICKS_IN_WINDOW = TICKS_IN_SECOND * SECONDS_IN_WINDOW;
@@ -17,47 +9,78 @@ STIMULI_PER_SECOND = 1/30;
 STIMULUS_EACH_TICKS = floor(TICKS_IN_SECOND*STIMULI_PER_SECOND); %333
 STIMULI_PER_WINDOW = SECONDS_IN_WINDOW / STIMULI_PER_SECOND; %3000 stims for 100 secs
 
-simulation.time= (StimTimeNonRepNorm(1)+1:1:StimTimeNonRepNorm(2)+1)'; %first 100 secs
+NEURONS = length(TTNonRep);
+ITERATIONS = length(StimTimeNonRep);
 
-actualWindowLength = StimTimeNonRep(2)-StimTimeNonRep(1)+1;
+SAFETY_SIZE_SUFFIX = 1000; %actually the max is 3725
 
-%window stim values by time
-stimValues= StimulusNonRep(1:STIMULI_PER_WINDOW);
-stimValues = repmat(stimValues,2,1);
-
-simulation.stimuli = NaN(actualWindowLength,1);
-stimTimes = 1:STIMULUS_EACH_TICKS:actualWindowLength;
-simulation.stimuli(stimTimes)=stimValues(1:length(stimTimes));
-
-onset = StimTimeNonRep(1)+1;
-next_onset = StimTimeNonRep(2)+1;
-
-timeOfAPs=TTNonRep(1).sp; %time of Aps
-
-%{
-        Change from time scale into discrete indexes and 
-        filter by AP occurences
+%window stim values by time, warming it up, to improve prefs
+globalStimValues = StimulusNonRep(1:STIMULI_PER_WINDOW);
+globalStimValues = repmat(globalStimValues,2,1);
         
-        logical() is required to transform binary vector to a valid filter.
-        %}
-indexes = 1:length(timeOfAPs);
-filter = logical(timeOfAPs(:,1) >= onset & timeOfAPs(:,1) < next_onset);
-indexesOfAPs = indexes(filter);
-
-%we have any APs
-%if(~isempty(indexesOfAPs))
+Neuron = cell(1,length(TTNonRep));
+for iNeuron = 1:length(TTNonRep)
     
-simulation.APs = zeros(actualWindowLength,1);
-%normalize the APs, to the start of window
-APsInWindow = timeOfAPs(indexesOfAPs)-onset;
-simulation.APs(APsInWindow)=1;
 
-%% finally, get
-all = zeros(actualWindowLength, 3);
-all(:,1) = simulation.time;
-all(:,2) = simulation.stimuli;
-all(:,3) = simulation.APs;
+    simulation.all = NaN((STIMULI_PER_WINDOW+SAFETY_SIZE_SUFFIX)*ITERATIONS, 3);
+    lastIterationIndex = 0;
+    for iIteration = 2:ITERATIONS
+        %fprintf('[N:#%i] processing iteration #%i/#%i ...\n', iNeuron, iIteration, ITERATIONS);
 
-simulation.all = all(all(:,2) > 0 | all(:,3) > 0,:);
+        %Normalize stim time to start from 1
+        iEnd = iIteration;
+        iStart = iEnd-1; 
+
+        StimTimeNonRepNorm=StimTimeNonRep-StimTimeNonRep(iStart);
+        iteration.time= (StimTimeNonRepNorm(iStart)+1:1:StimTimeNonRepNorm(iEnd)+1)'; %first 100 secs
+
+        actualWindowLength = StimTimeNonRep(iEnd)-StimTimeNonRep(iStart)+1;
+
+        
+        stimValues = globalStimValues(:);
+        iteration.stimuli = NaN(actualWindowLength,1);
+        stimTimes = 1:STIMULUS_EACH_TICKS:actualWindowLength;
+        iteration.stimuli(stimTimes)=stimValues(1:length(stimTimes));
+
+        timeOfAPs=TTNonRep(iNeuron).sp; %time of Aps
+
+        onset = StimTimeNonRep(iStart)+1;
+        next_onset = StimTimeNonRep(iEnd)+1;
+        %{
+                Change from time scale into discrete indexes and 
+                filter by AP occurences
+
+                logical() is required to transform binary vector to a valid filter.
+                %}
+        indexes = 1:length(timeOfAPs);
+        filter = logical(timeOfAPs(:) >= onset & timeOfAPs(:) < next_onset);
+        indexesOfAPs = indexes(filter);
+
+        %we have any APs
+        %if(~isempty(indexesOfAPs))
+
+        iteration.APs = zeros(actualWindowLength,1);
+        %normalize the APs, to the start of window
+        APsInWindow = timeOfAPs(indexesOfAPs)-onset + 1; %the index is 1 based
+        iteration.APs(APsInWindow)=1;
+
+        %create fixed data set
+        res = NaN(actualWindowLength, 3);
+        res(:,1) = iteration.time + onset;
+        res(:,2) = iteration.stimuli;
+        res(:,3) = iteration.APs;
+
+        iteration.all = res(res(:,2) > 0 | res(:,3) > 0,:);
+        nextIterationIndex = lastIterationIndex + length(iteration.all) + 1;
+        simulation.all(lastIterationIndex+1:nextIterationIndex-1,:) = iteration.all;
+        lastIterationIndex = nextIterationIndex+1;
+    end %iIteration
+
+    %remove NaN rows
+    Neuron{iNeuron} = simulation.all(any(~isnan(simulation.all),2),:);
+end %iNeuron
+
+
+
 
 
