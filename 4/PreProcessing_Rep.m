9b21da3e03b94c0c5a570c63b242c8c16b1df67d
shaybensasson <shaybensasson@gmail.com>
2015-02-09 08:43:08 +0200

    added PreProcRep to POCV2, tested plotafterpreproc

--- 4/PreProcessing_Rep.m
+++ 4/PreProcessing_Rep.m
@@ -1,30 +1,35 @@
 clear all;
-if (~exist('TTNonRep','var'))
+if (~exist('TTRep','var'))
     load('FixedData.mat')
+    
+    %fix diffs to be 200s instead of 100s, each time we load raw data
+    StimTimeRep = StimTimeRep(1:2:end); 
 end
 
 ConstantsHeader();
 
-SAVE_MAT_FILE = 1;
+SAVE_MAT_FILE = 0;
 
-%secs in stimulus non-rep window
-SECONDS_IN_WINDOW = 100;
+%secs in stimulus rep window
+SECONDS_IN_WINDOW = 200;
 TICKS_IN_SECOND = 10000;
 TICKS_IN_WINDOW = TICKS_IN_SECOND * SECONDS_IN_WINDOW;
 STIMULI_PER_SECOND = 1/30;
 %how many ticks there are between stimuli.
 STIMULUS_EACH_TICKS = TICKS_IN_SECOND*STIMULI_PER_SECOND; %333.333
-STIMULI_PER_WINDOW = SECONDS_IN_WINDOW / STIMULI_PER_SECOND; %3000 stims for 100 secs
+STIMULI_PER_WINDOW = SECONDS_IN_WINDOW / STIMULI_PER_SECOND; %6000 stims for 200 secs
 
-NEURONS = length(TTNonRep);
-ITERATIONS = length(StimTimeNonRep); %NOTE: each ITERATION is different bulk (non-repeating window)
-ITERATIONS = ITERATIONS-2; %we ignore the last 2 chunks because it has partial stimuli values
+NEURONS = length(TTRep);
+ITERATIONS = length(StimTimeRep); %NOTE: each ITERATION is a repetition
+
+%window stim values by time, warming it up, to improve prefs
+globalStimValues = StimulusRep(1:STIMULI_PER_WINDOW);
 
 clear Simulation;
-Simulation.Mode = CONSTANTS.MODES.NONREP;
+Simulation.Mode = CONSTANTS.MODES.REP;
 Simulation.Phase = CONSTANTS.PHASES.PREPROCESSING;
-Simulation.Neuron = cell(1,length(TTNonRep));
-Simulation.StimTimeNonRep = StimTimeNonRep;
+Simulation.Neuron = cell(1,length(TTRep));
+Simulation.StimTimeRep = StimTimeRep;
 Simulation.ITERATIONS = ITERATIONS;
 Simulation.SECONDS_IN_WINDOW = SECONDS_IN_WINDOW;
 Simulation.TICKS_IN_WINDOW = TICKS_IN_WINDOW;
@@ -40,46 +45,45 @@ for iNeuron = 2:2
     fprintf('[N:#%i] ...\n', iNeuron);
     
     %allocate an array to store prepocessed data for neuron
-    wholeExperiment = StimTimeNonRep(end) - StimTimeNonRep(1);
-    neuronData.all = NaN(wholeExperiment,3);
-
-    lastIterationIndex = 0;
-    lastIterationSimuliIndex = 0;
+    ESTIMATED_APS = STIMULI_PER_WINDOW;
+    ESTIMATE_NEURON_DATA_SIZE = (STIMULI_PER_WINDOW+ESTIMATED_APS)*ITERATIONS;
+    neuronData.all = NaN(ESTIMATE_NEURON_DATA_SIZE, 3);
     
+    lastIterationIndex = 0;
+
     for iIteration = 2:ITERATIONS
         %fprintf('[N:#%i] processing iteration #%i/#%i ...\n', iNeuron, iIteration, ITERATIONS);
 
         %Normalize stim time to start from 1
         iEnd = iIteration;
-        iStart = iEnd-1;
-        
+        iStart = iEnd-1; 
+
         %actual ticks in window limited by TICKS_IN_WINDOW
         dataTicks = TICKS_IN_WINDOW;
 
-        %Normalize stim time to start from 1
-        StimTimeNonRepNorm=StimTimeNonRep-StimTimeNonRep(iStart)+1;
-        iteration.time= (StimTimeNonRepNorm(iStart):StimTimeNonRepNorm(iEnd)-1)'; %first 100 secs
+        %Normalize stim time to start from 1        
+        StimTimeRepNorm=StimTimeRep-StimTimeRep(iStart)+1;
+        iteration.time= (StimTimeRepNorm(iStart):StimTimeRepNorm(iEnd)-1)'; %first 200 secs
         dataTicks = min(dataTicks,length(iteration.time)); 
         iteration.time = iteration.time(1:dataTicks); %trim
-        
-        %% get stim values of the current non-rep window        
+
+        %copy global var
+        stimValues = globalStimValues(:);
+
+        %% get stim values of the current rep window        
         iteration.stimuli = NaN(dataTicks,1);
 	
-        %create stim times for every stim
+        %create stim times for every stim        
         stimTimes = 1:floor(STIMULUS_EACH_TICKS):dataTicks;
         stimTimes = stimTimes(1:STIMULI_PER_WINDOW);
-        %get stim values on non-rep window, using stim times
-        nextIterationSimuliIndex = lastIterationSimuliIndex + length(stimTimes) + 1;
-        stimValues = StimulusNonRep(lastIterationSimuliIndex+1:nextIterationSimuliIndex-1,:);
-        lastIterationSimuliIndex = nextIterationSimuliIndex-1;
-                
+
         iteration.stimuli(stimTimes)=stimValues;
 
-        %% get aps of the current non-rep window        
-        timeOfAPs=TTNonRep(iNeuron).sp; %time of Aps
+        %% get aps of the current rep window        
+        timeOfAPs=TTRep(iNeuron).sp; %time of Aps
 
-        onset = StimTimeNonRep(iStart);
-        next_onset = StimTimeNonRep(iEnd);
+        onset = StimTimeRep(iStart);
+        next_onset = StimTimeRep(iEnd);
         %{
                 Change from time scale into discrete indexes and 
                 filter by AP occurences
@@ -116,5 +120,5 @@ end %iNeuron
 
 if (SAVE_MAT_FILE)
     fprintf('Saving simulation output ...\n');
-    save('PreProcessed_NonRep.mat', 'Simulation');
+    save('PreProcessed_Rep.mat', 'Simulation');
 end
